95da8dd9862761c0687693b0c6efed5e2f79c222
Brandon Green <brandon@brandonleeproductions.com>
2018-06-02 00:35:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(computers)
-  computers = { HP: 4, Dell: 19, Asus: 2 }
-  computers.each do |name, num|
-    if name 
+def key_for_min_value(things)
+    
 end
 
