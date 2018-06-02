3d19418d7a584ed048048b8ed13eaa588663ea19
Brandon Green <brandon@brandonleeproductions.com>
2018-06-02 00:45:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(things)
-  place_holder = 10000
-  if things.empty?
-    return nil 
-  else
-    things.each do |key, value|
-      place_holder > value
+def key_for_min_value(name_hash)
+  place_holder = 1000 
+
+  if name_hash.empty?
+   name = nil  
+  end
+
+  name_hash.each do |key,value|
+    if place_holder > value
       place_holder = value
-      things = key
+      name = key
     end
   end
-  things
+  name
 end
